73b0cf234f8969b9db0a13aadc072a774715d809
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-05-15 15:16:38 +0200

    Test for persist-file

--- test/profile2ddl/helper_test.clj
+++ test/profile2ddl/helper_test.clj
@@ -11,3 +11,10 @@
   (let [min -2147483648
         max 2147483647]
     (is (= (int-or-bigint min max) " INTEGER"))))
+
+
+(deftest test-persist-file
+  (let [data "This is DATA\n"
+        path "/tmp/profile2ddl-test-persist-file.txt"]
+    (persist-file path data)
+    (is (= (slurp path) data))))

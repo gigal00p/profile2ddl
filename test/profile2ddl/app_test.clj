b14a47f101d34e085683e6f019311635d5eedfa7
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-05-27 14:51:44 +0200

    Fix test-process-one-file

--- test/profile2ddl/app_test.clj
+++ test/profile2ddl/app_test.clj
@@ -55,7 +55,7 @@
 (deftest test-process-one-file
   (let [test-file-name "/tmp/profile2ddl/tests/process-one-file/test-csv-profile.csv"
         test-data "field,type,sum,min,max,min_length,max_length,mean,stddev\nSEQNUM NUMBER,Integer,5677245305943586,44,106596505,2,9,53296133.85074341,30778094.264473855\nVERSION,Unicode,,E1,E1,2,2,,"
-        expected-results "CREATE TABLE test-csv-profile (\n    seqnumnumber         INTEGER,\n    version              VARCHAR(2)\n);"
+        expected-results "CREATE TABLE test-csv-profile (\n    seqnumnumber         INTEGER,\n    version              VARCHAR(2)\n);\n"
         results-file-name "/tmp/profile2ddl/tests/process-one-file/test-csv-profile.ddl.sql"]
     (do
       (io/make-parents test-file-name)
